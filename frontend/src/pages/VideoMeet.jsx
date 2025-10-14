01edf8a21b0608fc03060b118c5399d1ec6d639b
Hemant <hemantschauhan042@gmail.com>
2025-10-14 13:18:20 +0530

    added camera feature

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -1,7 +1,8 @@
 const server_url = "http://localhost:8000";
 import {useState, useEffect} from 'react';
 import "../styles/videoComponent.css";
-
+import { useRef } from 'react';
+import { TextField, Button } from '@mui/material';
 
 
 var connections = {};
@@ -27,18 +28,68 @@ export default function VideoMeetComponent() {
     let [message, setMessage] = useState(""); //humne jo msg likhe
     let [newMessages, setNewMessages] = useState(0); //title wala msgs
     let [askForUsername, setAskForUsername] = useState(true); //guest
-    let [usersername, setUsername] = useState(""); 
+    let [username, setUsername] = useState(""); 
     const videoRef = useRef([]);
     let [videos, setVideos] = useState([]);
+    
 
     // if(isChrome() === false){
 
     // }
+
+    const getPermissions = async () => {
+        try {
+            const videoPermission = await navigator.mediaDevices.getUserMedia({video: true})  //camera permission
+            if(videoPermission) {
+                setVideoAvailable(true);
+            }else{
+                setVideoAvailable(false);
+            }
+
+            const audioPermission = await navigator.mediaDevices.getUserMedia({audio: true})  //camera permission
+            if(audioPermission) {
+                setAudioAvailable(true);
+            }else{
+                setAudioAvailable(false);
+            }
+
+            if(navigator.mediaDevices.getDisplayMedia) {
+                setScreenAvailable(true);
+            }else{
+                setScreenAvailable(false);
+            }
+
+            if(videoAvailable || audioAvailable) {
+                // woh media jo hum idhra udhr bhejenge
+                const userMediaStream = await navigator.mediaDevices.getUserMedia({video: videoAvailable, audio: audioAvailable});
+                if(userMediaStream) {
+                    // local stream humare cam ka
+                    window.localStream = userMediaStream;
+                    if(localVideoRef.current) {
+                        localVideoRef.current.srcObject = userMediaStream;
+                    }
+                }
+            }
+        }catch(err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(()=> {
+      getPermissions();  
+    },[])
     
     return (
         <div>
             {askForUsername === true ? 
                <div>
+                    <h2>Enter into Lobby</h2>
+                    <TextField id="outlined-basic" label="Username" value={username} onChange={e => setUsername(e.target.value)} />
+                    <Button variant="contained">Connect</Button>
+                    <div>
+                        <video ref={localVideoRef} autoPlay muted></video>
+                    </div>
+
 
                </div> :
                <div>
