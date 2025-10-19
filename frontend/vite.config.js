224d3da3959cb1f9da73088eb69337918860f3df
Hemant <hemantschauhan042@gmail.com>
2025-10-19 13:08:31 +0530

    bugs removed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',       // output folder
+    assetsDir: '',        // keep assets at root (optional)
+  },
+  publicDir: resolve(__dirname, 'public'), // make sure Vite knows where public files are
 })
